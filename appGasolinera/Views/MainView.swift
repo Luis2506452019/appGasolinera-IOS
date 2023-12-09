5c7e9241328f889fafc23b4bf097187b27ef00e1
Luis2506452019 <2506452019@mail.utec.edu.sv>
2023-12-09 07:50:04 -0800

    Summary

--- appGasolinera/Views/MainView.swift
+++ appGasolinera/Views/MainView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 struct MainView: View {
     @ObservedObject var authenticationViewModel: AuthenticationViewModel
-    
-    @State private var IndexSeleccionado = 2
+    @State private var indexSeleccionado = 2
     let iconos = ["gauge", "map", "house", "gift"]
 
     var body: some View {
+        
         NavigationView {
             VStack {
-                switch IndexSeleccionado {
+                // Contenido de navegación
+                switch indexSeleccionado {
                 case 0:
                     EstacionView()
                 case 1:
@@ -26,44 +27,47 @@ struct MainView: View {
                 case 3:
                     OfertaView()
                 default:
-                    MainView(authenticationViewModel: authenticationViewModel)
+                    EmptyView()
                 }
-
+                
+                // Menu Abajo
                 HStack {
                     ForEach(0..<4, id: \.self) { numeros in
-                        Spacer()
                         Button(action: {
-                            self.IndexSeleccionado = numeros
+                            self.indexSeleccionado = numeros
                         }, label: {
                             Image(systemName: iconos[numeros])
                                 .font(.system(size: 24))
                                 .frame(width: 60, height: 60)
                                 .background(
                                     RoundedRectangle(cornerRadius: 30)
-                                        .fill(numeros == IndexSeleccionado ? Color.white : ColorHelper.color("#333333"))
+                                        .fill(numeros == indexSeleccionado ? Color.white : ColorHelper.color("#333333"))
                                 )
                                 .cornerRadius(30)
-                                .foregroundColor(numeros == IndexSeleccionado ? .black : .white)
+                                .foregroundColor(numeros == indexSeleccionado ? .black : .white)
                         })
                     }
-                    Spacer()
-                }
+                }//Fin Hstack
             }
-            .background(ColorHelper.color("#333333"))
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    // Lado izquierdo del toolbar
-                }
-
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button("Logout"){
-                        authenticationViewModel.logout()
-                    }
-                }
-            }
-
+            //Para no colocar titulo
+            .navigationBarTitle("", displayMode: .inline)
             
-        }
+            .background(ColorHelper.color("#333333"))
+            
+            //Menu Arriba
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        authenticationViewModel.logout()
+                    }, label: {
+                        Text("Logout")
+                            .underline()
+                    })
+                    
+                }
+            }
+            
+        }.accentColor(ColorHelper.color("#333333"))
     }
 }
 
--- appGasolinera/Views/Screens/OfertaView.swift
+++ appGasolinera/Views/Screens/OfertaView.swift
@@ -9,61 +9,66 @@ import SwiftUI
 
 struct OfertaView: View {
     var body: some View {
-        ZStack {
-            ColorHelper.color("#333333").ignoresSafeArea()
-            VStack {
-                Text("Gasolineras UNO")
-                    .foregroundColor(.white)
-                    .font(.title3)
-                    .padding()
-                HStack {
-                    Image("uno")
-                        .resizable()
-                        .frame(width: 123, height: 132)
-                    VStack {
-                        Text("Obten el 20%\nen tu siguiente recarga")
-                            .foregroundColor(.white)
-                            .font(.system(size:16))
-                            .multilineTextAlignment(.center)
+        
+        ScrollView {
+            ZStack{
+                ColorHelper.color("#333333").ignoresSafeArea()
+                
+                VStack {
+                    Text("Gasolineras UNO")
+                        .foregroundColor(.white)
+                        .font(.title3)
+                        .padding()
+                    HStack {
+                        Image("uno")
+                            .resizable()
+                            .frame(width: 123, height: 132)
+                        VStack {
+                            Text("Obten el 20%\nen tu siguiente recarga")
+                                .foregroundColor(.white)
+                                .font(.system(size:16))
+                                .multilineTextAlignment(.center)
+                            Button(action: {
+                                //En Construccion...
+                            }) {
+                                Text("REGISTRA TU CUPON")
+                                    .foregroundColor(.white)
+                                    .fontWeight(.bold)
+                                    .font(.system(size:15))
+                                    .padding(8)
+                                    .background(Color.green)
+                            }
+                        }
+                    }
+                    Text("Mas ofertas en:")
+                        .foregroundColor(.white)
+                        .font(.title3)
+                        .padding()
+                    
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .frame(width: 240, height: 3)
+                    
+                    HStack {
                         Button(action: {
                             //En Construccion...
                         }) {
-                            Text("REGISTRA TU CUPON")
-                                .foregroundColor(.white)
-                                .fontWeight(.bold)
-                                .font(.system(size:15))
-                                .padding(8)
-                                .background(Color.green)
+                            Image("puma")
+                                .resizable()
+                                .frame(width: 111, height: 117)
+                                .padding()
+                        }
+                        Button(action: {
+                            //En Construccion...
+                        }) {
+                            Image("texaco")
+                                .resizable()
+                                .frame(width: 109, height: 134)
+                                .padding()
                         }
                     }
-                }
-                Text("Mas ofertas en:")
-                    .foregroundColor(.white)
-                    .font(.title3)
-                    .padding()
+                }//.padding()
                 
-                Rectangle()
-                    .foregroundColor(.black)
-                    .frame(width: 240, height: 3)
-                
-                HStack {
-                    Button(action: {
-                        //En Construccion...
-                    }) {
-                        Image("puma")
-                            .resizable()
-                            .frame(width: 111, height: 117)
-                            .padding()
-                    }
-                    Button(action: {
-                        //En Construccion...
-                    }) {
-                        Image("texaco")
-                            .resizable()
-                            .frame(width: 109, height: 134)
-                            .padding()
-                    }
-                }
             }
         }
 
